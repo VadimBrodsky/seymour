3cdb214c0c48ee0e363dac9e7e2a6243cbaa9285
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-07-22 11:20:51 -0400

    Add feeds

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Feeds from './Feeds';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar />
+        <div className="container">
+          <Feeds />
+        </div>
       </Fragment>
     );
   }
---
+++ src/components/Feeds.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <ul className="list-group">
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      Awesome Site
+      <span className="badge badge-primary badge-pill">2</span>
+    </li>
+  </ul>
+);
