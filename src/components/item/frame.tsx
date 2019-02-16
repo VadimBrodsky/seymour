3c8c232c77fb1ac3119c456765084a73417c11f1
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-02-16 14:37:55 -0500

    Change the base to open popups in a new tab, start on iframe csp

--- src/components/item/frame.tsx
+++ src/components/item/frame.tsx
@@ -10,14 +10,20 @@ export default function Frame({ markup }: { markup: string }) {
       iframeDoc.open();
       iframeDoc.write(markup);
       iframeDoc.close();
+
+      // change the base to have links open in a new tab
+      const base = document.createElement('base');
+      base.target = "_blank";
+      iframeDoc.head.appendChild(base);
     }
   }, []);
 
   return (
     // @ts-ignore
     <iframe
+      csp="default-src 'self'"
       allowtransparency="allowtransparency"
-      sandbox="allow-same-origin"
+      sandbox="allow-same-origin allow-popups"
       ref={iframeEl}
     />
   );
