e73cbb487620eab953a6cc03e219faa721f7ee3d
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-02-23 10:46:01 -0500

    Hook up article selection

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import fetcher from './services/fetcher';
 import rssParser, { Channel } from './services/rss-parser';
 import Content from './components/item/content';
+import FeedList from './components/feed/list';
 
 export default function App() {
   const [feed, setFeed] = useState({} as Channel);
@@ -14,9 +15,15 @@ export default function App() {
     });
   }, []);
 
+  const [article, setArticle] = useState('');
+
   if (!feed.channel || !feed.channel.items) {
     return <p>...</p>;
   }
+
+  const selectedArticle = feed.channel.items.find(
+    (item) => (item.guid === article ? true : false),
+  );
 
   return (
     <main className="flex h-screen">
@@ -28,25 +35,20 @@ export default function App() {
       <section className="w-1/3 bg-grey-light overflow-y-auto scroling-touch p-3">
         <header>
           <h1 className="text-lg">Overreacted</h1>
+          <FeedList items={feed.channel.items} onSelect={(item) => setArticle(item)} />
         </header>
-        <ul className="list-reset">
-          {feed.channel.items.map((item) => (
-            <li
-              key={item.guid}
-              className="py-3 text-sm border-b-2 border-grey border-solid"
-            >
-              <a href="#" className="no-underline text-black">
-                {item.title}
-              </a>
-            </li>
-          ))}
-        </ul>
       </section>
       <article className="w-1/2 bg-white flex flex-col">
         <header className="p-3">
-          <h1 className="text-lg">{feed.channel.items[0].title}</h1>
+          <h1 className="text-lg">
+            {selectedArticle ? selectedArticle.title : feed.channel.items[0].title}
+          </h1>
         </header>
-        <Content markup={feed.channel.items[0].content} />
+        <Content
+          markup={
+            selectedArticle ? selectedArticle.content : feed.channel.items[0].content
+          }
+        />
       </article>
     </main>
   );
