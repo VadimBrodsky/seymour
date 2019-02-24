883b7bb6bcc907ef95ca894a9a532110184eb51b
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-02-23 23:15:01 -0500

    Add the Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import fetcher from './services/fetcher';
-import rssParser, { Channel } from './services/rss-parser';
-import Content from './components/item/content';
-import FeedList from './components/feed/list';
+import rssParser from './services/rss-parser';
+
+import Chrome from './components/chrome';
+import Feed from './components/feed';
+import Article from './components/article';
 
 type Article = ArrayElement<Channel['channel']['items']>;
 
@@ -34,28 +37,24 @@ export default function App() {
     ? selectedArticle
     : feed.channel.items[0];
 
+  const handleSelectedFeed = (item: string) => setSelecedArticleId(item);
+
   return (
-    <main className="flex h-screen">
-      <nav className="w-1/6 bg-grey overflow-y-auto overflow-x-hidden p-3">
-        <header>
-          <h1 className="text-lg">Seymour</h1>
-        </header>
-      </nav>
-      <section className="w-1/3 bg-grey-light overflow-y-auto scroling-touch p-3">
-        <header>
-          <h1 className="text-lg">Overreacted</h1>
-          <FeedList
-            items={feed.channel.items}
-            dispatchSelect={(item) => setSelecedArticleId(item)}
-          />
-        </header>
-      </section>
-      <article className="w-1/2 bg-white flex flex-col">
-        <header className="p-3">
-          <h1 className="text-lg">{currentArticle.title}</h1>
-        </header>
-        <Content markup={currentArticle.content} />
-      </article>
-    </main>
+    <Router>
+      <Chrome>
+        <Route
+          path="/feed"
+          component={() => (
+            <Feed feedItems={feed.channel.items} dispatchSelect={handleSelectedFeed} />
+          )}
+        />
+        <Route
+          path="/feed/:id"
+          component={() => (
+            <Article title={currentArticle.title} content={currentArticle.content} />
+          )}
+        />
+      </Chrome>
+    </Router>
   );
 }
