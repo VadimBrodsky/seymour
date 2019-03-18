d73f8bf3ebfafac62bc41df059c497a285a773c9
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-03-18 02:27:18 +0000

    Create store and add provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './stylesheets/tailwind.css';
 import App from './App';
+import reducer from './reducers';
+import middleware from './middleware';
 import * as serviceWorker from './serviceWorker';
 import seedData from './services/seed';
 
 //@ts-ignore
 window.seedData = seedData;
+const store = createStore(reducer, middleware);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
