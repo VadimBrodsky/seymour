ea95ce14d17b0a77f6139a9e34735170a9b8530f
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-03-26 22:18:57 -0400

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { handleReceiveChannels } from './actions/channels';
-import { handleReceiveItems } from './actions/items';
 import Article from './components/article';
 import Chrome from './components/chrome';
-import FeedMenu from './components/feed';
-import Loading from './components/shared/loading';
+import Feeds from './components/feed';
 import Sidebar from './components/sidebar';
-import { AppState, RootActions } from './reducers';
+import { AppState } from './reducers';
 
 interface Props {
   channels: AppState['channels']['loaded'];
@@ -21,15 +19,11 @@ function App({ channels, dispatch }: Props) {
     dispatch(handleReceiveChannels());
   }, []);
 
-  if (!channels) {
-    return <Loading />;
-  }
-
   return (
     <Router>
       <Chrome>
         <Route path="/" component={Sidebar} />
-        <Route path="/channel/:feedId" component={FeedMenu} />
+        <Route path="/channel/:feedId" component={Feeds} />
         <Route path="/channel/:feedId/:articleId" component={Article} />
       </Chrome>
     </Router>
