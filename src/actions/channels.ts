9229db7021b53b638055e1df88dd94dc24d0f244
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-03-28 22:53:18 -0400

    Start on redux actions for channel fetching

--- src/actions/channels.ts
+++ src/actions/channels.ts
@@ -1,14 +1,28 @@
 import { Action } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
 import db from '../services/db';
+import fetcher from '../services/fetcher';
+import rssParser from '../services/rss-parser';
 
 // Actions
 export const RECEIVE_CHANNELS = 'RECEIVE_CHANNELS';
+export const SUBSCRIBE_TO_CHANNEL = 'SUBSCRIBE_TO_CHANNEL';
+export const SUBSCRIBTION_ERROR = 'SUBSCRIBTION_ERROR';
 
 // Action Creators
 export const receiveChannels = (channels: State['loaded']) => ({
   type: RECEIVE_CHANNELS,
   channels,
+});
+
+export const subscribeToChannel = (channel: State['loaded'][0]) => ({
+  type: SUBSCRIBE_TO_CHANNEL,
+  channel,
+});
+
+export const subscribtionError = (message: string) => ({
+  type: SUBSCRIBTION_ERROR,
+  message,
 });
 
 // Thunks
@@ -19,6 +33,16 @@ export const handleReceiveChannels = () => {
     dispatch(receiveChannels(channels));
   };
 };
+
+export const handleSubscribeToChannel = (url: string) => {
+  return async (dispatch: any) => {
+    try {
+      const data = await fetcher(url);
+    } catch(e) {
+      dispatch(subscribtionError(e.message));
+    }
+  }
+}
 
 // Types
 export interface State {
