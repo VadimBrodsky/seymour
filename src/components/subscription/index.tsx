cabc66a6e3ef7b70dbed6e0bd1e334b8142bda98
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-03-29 23:27:04 -0400

    Use redux instead of local state

--- src/components/subscription/index.tsx
+++ src/components/subscription/index.tsx
@@ -1,53 +1,38 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import db from '../../services/db';
 import fetcher from '../../services/fetcher';
 import rssParser from '../../services/rss-parser';
+import { AppState } from '../../reducers';
+import { handleLoadChannel, handleSubscribeToChannel } from '../../actions/channels';
 
-function Subscription() {
+interface Props {
+  dispatch: (action: any) => any;
+  error?: AppState['channels']['newChannelError'];
+  itemsLength?: number;
+  lastPubDate?: string;
+  subscription?: AppState['channels']['newChannel'];
+}
+
+function Subscription(props: Props) {
   const inputRef = React.useRef(null);
-  const [error, setError] = React.useState(null);
-
-  const subscribe = async (url: string) => {
-    try {
-      const data = await fetcher(url);
-      console.log(data);
-      if (data) {
-        const feed = rssParser(data);
-        console.log(feed);
-
-        if (!feed.title || !feed.link) {
-          throw new Error('Could not parse the XML feed');
-        }
-
-        await db.channels.add({
-          title: feed.title,
-          feedUrl: url,
-          slug: feed.slug,
-          description: feed.description,
-          link: feed.link,
-          lastBuildDate: feed.lastBuildDate,
-          lastFetched: Date.now(),
-        });
-
-        // @ts-ignore
-        inputRef.current.value = '';
-
-        // @ts-ignore
-        window.syncFeedsWorker();
-      }
-    } catch (e) {
-      setError(e.message);
-    }
-  };
+  const [url, setUrl] = React.useState('');
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setError(null);
     // @ts-ignore
     const val = inputRef.current.value;
+    if (val !== '') {
+      props.dispatch(handleLoadChannel(val));
+      setUrl(val);
+    }
+  };
 
-    console.log(val);
-    val !== '' && subscribe(val);
+  const handleSubscribe = () => {
+    // @ts-ignore
+    inputRef.current.value = '';
+    props.subscription &&
+      props.dispatch(handleSubscribeToChannel(props.subscription, url));
   };
 
   return (
@@ -68,13 +53,51 @@ function Subscription() {
           className="bg-blue shadow-md hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
         >
-          Subscribe
+          Load Feed
         </button>
 
-        {error && <p className="text-red text-xs italic">{error}</p>}
+        {props.error && <p className="text-red text-xs italic">{props.error}</p>}
       </form>
+
+      {props.subscription && (
+        <>
+          <h2>{props.subscription.title}</h2>
+          <p>{props.subscription.link}</p>
+          <p>{props.subscription.description}</p>
+          <p>
+            feed has {props.itemsLength} items, last one posted on {props.lastPubDate}
+          </p>
+          <button
+            onClick={handleSubscribe}
+            className="bg-blue shadow-md hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Subscribe
+          </button>
+        </>
+      )}
     </section>
   );
 }
 
-export default Subscription;
+const mapStateToProps = ({ channels: { newChannel, newChannelError } }: AppState) => {
+  let itemsLength;
+  let lastPubDate;
+
+  if (newChannel) {
+    itemsLength = newChannel.items ? newChannel.items.length : undefined;
+  }
+
+  if (newChannel && newChannel.items) {
+    lastPubDate = new Date(newChannel.items[0].pubDate).toLocaleDateString();
+  }
+
+  return {
+    error: newChannelError,
+    itemsLength,
+    lastPubDate,
+    subscription: newChannel,
+  };
+};
+
+export default connect(mapStateToProps)(Subscription);
