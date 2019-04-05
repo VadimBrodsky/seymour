04edb70bfc6f01aba00af0f91880b23275530af9
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-04-04 21:54:05 -0400

    Prettier

--- src/reducers/shared.ts
+++ src/reducers/shared.ts
@@ -1,9 +1,9 @@
-import {SHARED_ACTION} from '../actions/shared';
+import { SHARED_ACTION } from '../actions/shared';
 
 export default function sharedReducer(state: any, action: any) {
   switch (action.type) {
     case SHARED_ACTION:
-      return {...state}
+      return { ...state };
     default:
       return state;
   }
