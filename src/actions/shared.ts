1b1233de4fe0ceff542d8ee73fd4308509a666a0
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-04-04 21:53:31 -0400

    Add a shared reducer and action stub

---
+++ src/actions/shared.ts
@@ -0,0 +1,1 @@
+export const SHARED_ACTION = 'SHARED_ACTION';
---
+++ src/reducers/shared.ts
@@ -0,0 +1,10 @@
+import {SHARED_ACTION} from '../actions/shared';
+
+export default function sharedReducer(state: any, action: any) {
+  switch (action.type) {
+    case SHARED_ACTION:
+      return {...state}
+    default:
+      return state;
+  }
+}
